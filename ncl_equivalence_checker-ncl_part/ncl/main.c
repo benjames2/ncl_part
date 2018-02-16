8f69a0b78a092267141780075c512bef74360955
JulianThrash <32520178+JulianThrash@users.noreply.github.com>
2018-02-15 19:32:32 -0600

    added comments

--- ncl_equivalence_checker-ncl_part/ncl/main.c
+++ ncl_equivalence_checker-ncl_part/ncl/main.c
@@ -1,3 +1,13 @@
+//ECE 405: Senior Design III
+//NCL Equivalence Checker
+//Created by James Campbell, Dylan Visto, Julian Thrash, Ben Trnka
+
+
+//NOTE: User must provide an inputs list and netlist (circuit list) for this code to properly work
+//This code creates the NCL SMT statements for use by Z3 in proving equivalence between NCL circuit and Synchronous
+
+//Include header files and standard library files
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
